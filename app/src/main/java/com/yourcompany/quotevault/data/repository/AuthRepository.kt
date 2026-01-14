34fa71404d1dac373f6acd7b7c856fd080201864
pratish444 <pratishk444@gmail.com>
2026-01-14 20:00:59 +0530

    Added authentication feature with Supabase

--- app/src/main/java/com/yourcompany/quotevault/data/repository/AuthRepository.kt
+++ app/src/main/java/com/yourcompany/quotevault/data/repository/AuthRepository.kt
@@ -1,2 +1,18 @@
 package com.yourcompany.quotevault.data.repository
 
+import com.yourcompany.quotevault.domain.model.User
+import com.yourcompany.quotevault.domain.model.UserPreferences
+import com.yourcompany.quotevault.utils.Result
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+    val currentUser: Flow<User?>
+    suspend fun signUp(email: String, password: String, displayName: String): Result<User>
+    suspend fun signIn(email: String, password: String): Result<User>
+    suspend fun signOut(): Result<Unit>
+    suspend fun resetPassword(email: String): Result<Unit>
+    suspend fun resendConfirmationEmail(email: String): Result<Unit>
+    suspend fun getCurrentUser(): User?
+    suspend fun syncUserPreferences(): Result<UserPreferences>
+    suspend fun uploadUserPreferences(): Result<Unit>
+}
