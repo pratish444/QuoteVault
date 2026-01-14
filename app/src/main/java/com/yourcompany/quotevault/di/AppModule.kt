47ba5bd48640ffc144e39ddda18c0d4101cd986a
pratish444 <pratishk444@gmail.com>
2026-01-14 22:30:47 +0530

    Add AppModule with NetworkMonitor provider

---
+++ app/src/main/java/com/yourcompany/quotevault/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.yourcompany.quotevault.di
+
+import android.content.Context
+import com.yourcompany.quotevault.utils.NetworkMonitor
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNetworkMonitor(
+        @ApplicationContext context: Context
+    ): NetworkMonitor {
+        return NetworkMonitor(context)
+    }
+}
