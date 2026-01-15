9da2c6df9256f5798c065b908344270b53307492
pratish444 <pratishk444@gmail.com>
2026-01-16 00:24:58 +0530

    Create the Room database for Quote Vault

--- app/src/main/java/com/yourcompany/quotevault/data/local/QuoteVaultDatabase.kt
+++ app/src/main/java/com/yourcompany/quotevault/data/local/QuoteVaultDatabase.kt
@@ -1,2 +1,28 @@
 package com.yourcompany.quotevault.data.local
 
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.yourcompany.quotevault.data.local.dao.CollectionDao
+import com.yourcompany.quotevault.data.local.dao.FavoriteDao
+import com.yourcompany.quotevault.data.local.dao.QuoteDao
+import com.yourcompany.quotevault.data.local.dao.QuoteCacheDao
+import com.yourcompany.quotevault.data.local.entities.*
+
+@Database(
+    entities = [
+        QuoteEntity::class,
+        FavoriteEntity::class,
+        CollectionEntity::class,
+        CollectionQuoteEntity::class,
+        QuoteCacheEntity::class
+    ],
+    version = 2,
+    exportSchema = false
+)
+abstract class QuoteVaultDatabase : RoomDatabase() {
+    abstract fun quoteDao(): QuoteDao
+    abstract fun favoriteDao(): FavoriteDao
+    abstract fun collectionDao(): CollectionDao
+    abstract fun quoteCacheDao(): QuoteCacheDao
+}
